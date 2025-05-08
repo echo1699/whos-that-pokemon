fcbcf54dbcec4309a049a84110b0beedd997c523
echo1699 <echoecho1699@gmail.com>
2025-05-08 00:00:11 -0500

    Added a capitalize component and added a text element to that is visible when the answer is correct.

--- whos-that-pokemon/src/components/Game.tsx
+++ whos-that-pokemon/src/components/Game.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { PokemonType } from '../types/Pokemon.types'
 import AnswerInput from './AnswerInput'
+import { Capitalize } from './Capitalize'
 
 interface GameProps {
   pkm: PokemonType;
@@ -14,10 +15,11 @@ const Game:React.FC<GameProps> = ({ pkm, crrtPkm, setCrrtPkm }) => {
   if (pkm.id === crrtPkm) {
     return (
       <>
-        <div className='flex flex-col items-center justify-center h-screen bg-red-500 font-bold text-black'>
+        <div className='flex flex-col items-center justify-center h-screen font-bold text-black'>
           <h1 className='text-4xl'>Who's that Pokemon!</h1>
           <div className='flex flex-col items-center justify-center'>
             <img src={pkm.sprites.front} className={isCorrect ? 'w-md h-auto max-w-lg' : 'w-md h-auto max-w-lg grayscale brightness-0'} />
+            <h2 className={isCorrect ? 'text-2xl mb-4 visible' : 'invisible'}>Correct! It's {Capitalize(pkm.name)}!</h2>
             <AnswerInput name={pkm.name} setIsCorrect={setIsCorrect} />
             <button 
               className='bg-white text-red-500 font-bold px-4 py-2 rounded cursor-pointer mt-4'
